f5b86a832dbea96e3de0eda524978731b440161e
Mohammed Arief <moa@captureone.com>
2020-10-20 16:32:03 +0200

    Test Video

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,62 +1,23 @@
-import Layout from "../components/Layout";
-import BasicMeta from "../components/meta/BasicMeta";
-import OpenGraphMeta from "../components/meta/OpenGraphMeta";
-import TwitterCardMeta from "../components/meta/TwitterCardMeta";
-import { SocialList } from "../components/SocialList";
+import ReactPlayer from "react-player/lazy";
 
 export default function Index() {
   return (
-    <Layout>
-      <BasicMeta url={"/"} />
-      <OpenGraphMeta url={"/"} />
-      <TwitterCardMeta url={"/"} />
-      <div className="container">
-        <div>
-          <h1>
-            Hi, We're Next.js fwefwe fwe fwe wef wef wef wef wef& Netlify<span className="fancy">.</span>
-          </h1>
-          <span className="handle">@nextjs-netlify-blog</span>
-          <h2>A blog template with Next.js and Netlify.</h2>
-          <SocialList />
-        </div>
-      </div>
-      <style jsx>{`
-        .container {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex: 1 1 auto;
-          padding: 0 1.5rem;
-        }
-        h1 {
-          font-size: 2.5rem;
-          margin: 0;
-          font-weight: 500;
-        }
-        h2 {
-          font-size: 1.75rem;
-          font-weight: 400;
-          line-height: 1.25;
-        }
-        .fancy {
-          color: #15847d;
-        }
-        .handle {
-          display: inline-block;
-          margin-top: 0.275em;
-          color: #9b9b9b;
-          letter-spacing: 0.05em;
-        }
+    <div className="video-player">
+      <ReactPlayer
+        playing={true}
+        muted
+        width="100%"
+        height="auto"
+        controls={true}
+        url={["/assets/videos/Capture One.mp4"]}
+      />
 
-        @media (min-width: 769px) {
-          h1 {
-            font-size: 3rem;
-          }
-          h2 {
-            font-size: 2.25rem;
-          }
+      <style jsx>{`
+        .video-player {
+          
+          height: calc(100% - 200px);
         }
-      `}</style>
-    </Layout>
+        `}</style>
+    </div>
   );
 }
