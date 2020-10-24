8378599b648516a6dd811a39b82f2d1a2bee0778
Mohammed Arief <moa@captureone.com>
2020-10-25 00:05:12 +0200

    Video player

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import { AppProps } from "next/app";
 // NOTE: Do not move the styles dir to the src.
 // They are used by the Netlify CMS preview feature.
 import "../../public/styles/global.css";
+import '../../node_modules/video-react/dist/video-react.css';
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,81 @@
 import ReactPlayer from "react-player/lazy";
 
+import { Player, BigPlayButton } from "video-react";
+import { useEffect, useRef, useState } from "react";
+
 export default function Index() {
+  const [playerState, setPLayerState] = useState({});
+  const [vplayerState, setVPLayerState] = useState(false);
+
+  const play = () => {
+    setVPLayerState(true);
+    playerState.actions.mute(false);
+    playerState.actions.seek(0);
+    playerState.actions.play();
+  };
+
+  const stop = () => {
+    setVPLayerState(false);
+    playerState.actions.mute(true);
+  };
+
   return (
-    <div className="video-player">
-      <ReactPlayer
-        playing={true}
-        muted
-        width="100%"
-        height="auto"
-        controls={true}
-        url={["/assets/videos/Capture One.mp4"]}
-      />
+    <div className="test">
+      <div className="video-player">
+        <Player
+          ref={(player) => {
+            setPLayerState(player);
+          }}
+          fluid
+          muted={true}
+          playsInline={true}
+          autoPlay={true}
+          width={10}
+        >
+          <BigPlayButton position="center" />
+          <source src={"/assets/videos/Capture One.mp4"} />
+        </Player>
+
+        <div className="overlay"></div>
+        <div className="overlay-content">
+          <button onClick={() => play()}>Play</button>
+          <button onClick={() => stop()}>Stop</button>
+        </div>
+      </div>
 
       <style jsx>{`
-        .video-player {
-          
-          height: calc(100% - 200px);
+        .test {
+          display: flex;
+          justify-content: center;
+          width: 100%;
+          position: relative;
         }
-        `}</style>
+
+        .video-player {
+          width: 60%;
+        }
+
+        .overlay {
+          display: ${vplayerState ? "none" : "block"};
+          width: 100%;
+          position: absolute;
+          top: 0;
+          height: 100%;
+          left: 0;
+          background-color: rgba(12, 13, 17, 0.75);
+        }
+
+        .overlay-content {
+          width: 100%;
+          position: absolute;
+          top: 0;
+          height: 100%;
+          left: 0;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        }
+      `}</style>
     </div>
   );
 }
